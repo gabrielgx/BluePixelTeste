b2a3d249357d5a457559e6d2e2a5ed506e2fef87
Marcos Gabriel <marcosgabrielgx@gmail.com>
2019-12-29 19:29:16 -0300

    Adicionado musica de fundo, criado menu inicial e menu de pause

---
+++ Assets/Scripts/Menus.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menus : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public GameObject menuInicial;
+    public static bool gameIsPause;
+    private bool jogoComecou;
+    void Start()
+    {
+        jogoComecou=false;
+        menuInicial.SetActive(true);
+        pauseMenu.SetActive(false);
+        Time.timeScale = 0f;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (jogoComecou)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (gameIsPause)
+                {
+                    Play();
+                }
+                else
+                {
+                    Pause();
+                }
+            }
+
+        }
+
+    }
+
+   
+    public void IniciarJogo()
+    {
+        menuInicial.SetActive(false);
+        jogoComecou = true;
+        Time.timeScale = 1f;
+
+    }
+    
+    public void Play()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPause = false;
+
+    }
+
+    void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPause = true;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Fase1");
+        Time.timeScale = 1f;
+    }
+
+    public void Sair()
+    {
+        Application.Quit();
+    }
+}
